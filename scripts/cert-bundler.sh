e37aab08a282cdb333034d0b34a2b251c59ec449
cgmckeever <cgmckeever@ironhide.local>
2024-11-09 07:13:03 -0600

    cert bundle include the file dir

--- scripts/cert-bundler.sh
+++ scripts/cert-bundler.sh
@@ -6,6 +6,7 @@ source ${SCRIPT_PATH}/inc/functions.sh
 conf ${1}
 
 TAK_CERT_BUNDLE=${TAK_ALIAS}.tak.certs.zip
+BUNDLE_BACKUP="${TAK_ALIAS}.tak.certs.$(date +%s).zip"
 
 BUNDLE="${RELEASE_PATH}/${TAK_CERT_BUNDLE}"
 cd ${RELEASE_PATH}/tak/certs/
@@ -15,7 +16,7 @@ msg $info "\nCreated cert bundle: ${BUNDLE}"
 mkdir -p ${ROOT_PATH}/release/cert-backup
 
 if [ "${2}" != "false" ]; then
-    cp -f ${BUNDLE} ${ROOT_PATH}/release/cert-backup/${TAK_CERT_BUNDLE}.$(date +%s)
+    cp -f ${BUNDLE} ${ROOT_PATH}/release/cert-backup/${BUNDLE_BACKUP}
 fi
 
 echo
